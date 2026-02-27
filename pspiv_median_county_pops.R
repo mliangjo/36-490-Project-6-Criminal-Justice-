008f03e5691c642862e6c91a210c555b37ee1198
pspivack <pspivack@andrew.cmu.edu>
2026-02-27 09:35:59 -0500

    Also checked median household income

--- pspiv_median_county_pops.R
+++ pspiv_median_county_pops.R
@@ -1,14 +1,16 @@
 library(tidyverse)
 
+# fu <- read_csv("data/raw/Florida_State_Data/measures.csv")
+
 fl <- read_csv("data/raw/Florida_State_Data/data-2009-2013-fl.csv")
 or <- read_csv("data/raw/Oregon_State_Data/data-2009-2013-or.csv")
 pa <- read_csv("data/raw/Pennsylvania_State_Data/data-2009-2013.csv")
 
 fl %>%
-  # Measure 501 is population 
+  # Measure 501 is population, 
   # Location_id = FL gets rid of the statewide data
   filter(measure_id == 501, location_id != "FL") %>%
-  summarize(med = median(value))
+  summarize(pop = median(value))
 
 or %>%
   # Measure 501 is population 
@@ -20,4 +22,24 @@ pa %>%
   # Measure 501 is population 
   # Location_id = FL gets rid of the statewide data
   filter(measure_id == 501, location_id != "PA") %>%
+  summarize(med = median(value))
+
+# Med Household Income
+
+fl %>%
+  # Measure 525 is median household income, 
+  # Location_id = FL gets rid of the statewide data
+  filter(measure_id == 525, location_id != "FL") %>%
+  summarize(pop = median(value))
+
+or %>%
+  # Measure 501 is population 
+  # Location_id = FL gets rid of the statewide data
+  filter(measure_id == 525, location_id != "OR") %>%
+  summarize(med = median(value))
+
+pa %>%
+  # Measure 501 is population 
+  # Location_id = FL gets rid of the statewide data
+  filter(measure_id == 525, location_id != "PA") %>%
   summarize(med = median(value))
